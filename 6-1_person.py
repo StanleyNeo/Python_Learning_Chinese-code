9027170bda2bd8e87ada100120896aaac34153fe
SeeKwee <108121189+tuahuat999@users.noreply.github.com>
2025-06-11 15:49:48 +0800

    6-1_个人信息 20250611 SKNEO

---
+++ 6-1_person.py
@@ -0,0 +1,23 @@
+个人信息 = {
+    '名': '小明',
+    '姓': '张',
+    '年龄': 35,
+    '城市': '北京',
+}
+
+print(个人信息['名'])
+print(个人信息['姓'])
+print(个人信息['年龄'])
+print(个人信息['城市'])
+
+person = {
+    'first_name': 'eric',
+    'last_name': 'matthes',
+    'age': 43,
+    'city': 'sitka',
+    }
+
+print(person['first_name'])
+print(person['last_name'])
+print(person['age'])
+print(person['city'])
