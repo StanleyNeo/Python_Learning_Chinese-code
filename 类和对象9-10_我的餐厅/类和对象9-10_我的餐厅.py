fb476277ea100a9b20085ee11dcb2020f63272f6
SeeKwee <108121189+tuahuat999@users.noreply.github.com>
2025-06-19 11:36:29 +0800

    类和对象9-10_我的餐厅 20250619 Stanley Neo

---
+++ 类和对象9-10_我的餐厅/类和对象9-10_我的餐厅.py
@@ -0,0 +1,7 @@
+# 9-10_my_restaurant 类和对象 我的餐厅 20250619 Stanley Neo
+
+from 我的餐厅 import Restaurant
+
+channel_club = Restaurant('the channel club', 'steak and seafood')
+channel_club.describe_restaurant()
+channel_club.open_restaurant()
